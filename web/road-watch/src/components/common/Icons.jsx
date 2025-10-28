521ec40664bfaa7ca16546c33256ca2b745efc11
Kyle <kyle.sepulveda27@gmail.com>
2025-10-28 19:26:35 +0800

    feat(admin): create confirmation modal (revoke, suspend, activate)

--- web/road-watch/src/components/common/Icons.jsx
+++ web/road-watch/src/components/common/Icons.jsx
@@ -479,3 +479,9 @@ export const XIcon = () => (
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
   </svg>
 );
+
+export const LogoutIconAlt = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+  </svg>
+);
