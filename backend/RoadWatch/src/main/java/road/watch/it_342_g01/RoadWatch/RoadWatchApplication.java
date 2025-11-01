b3f2772289e19dbbf4706c2ee5f2467566d11658
Kyle <kyle.sepulveda27@gmail.com>
2025-11-01 22:44:16 +0800

    setup(backend): Setting up Backend Springboot

---
+++ backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/RoadWatchApplication.java
@@ -0,0 +1,13 @@
+package road.watch.it_342_g01.RoadWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadWatchApplication.class, args);
+	}
+
+}
---
+++ backend/RoadWatch/src/test/java/road/watch/it_342_g01/RoadWatch/RoadWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package road.watch.it_342_g01.RoadWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
