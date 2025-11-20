90ced740b517b5d4ecc1a24d482a8f7465fbc6db
Mortymier <sanielmg@gmail.com>
2025-11-21 00:20:45 +0800

    Backend: add function for getting all reports by name in ReportService2

--- backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/service/ReportService2.java
+++ backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/service/ReportService2.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import road.watch.it_342_g01.RoadWatch.entity.ReportEntity;
 import road.watch.it_342_g01.RoadWatch.repository.ReportRepo;
+import java.util.List;
 
 @Service
 public class ReportService2
@@ -16,5 +17,11 @@ public class ReportService2
     {
         report.setSubmittedBy(submittedBy);
         return reportRepo.save(report);
+    }
+
+    // Get all reports by name (submittedBy)
+    public List<ReportEntity> getAllReportsByName(String submittedBy)
+    {
+        return reportRepo.findBySubmittedBy(submittedBy);
     }
 }
