dbeb9932b861cc20d6542e14768cfed4bf7fddb5
Mortymier <sanielmg@gmail.com>
2025-11-20 13:14:44 +0800

    Backend: update security config for new endpoint

--- backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/config/SecurityConfig.java
+++ backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         "/api/admin/add",
                         "/api/citizen/add",
                         "/api/inspector/add",
-                        "/api/reports/add",
+                        "/api/reports/**",
                         "/api/users/getAll",
                         "/api/reports/getAll",
                         "/api/users/db-test",  // Add this line
