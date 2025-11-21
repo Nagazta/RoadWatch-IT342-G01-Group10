1f6edbafbabc989d6e059ba56a6fd6f09c13b452
kyle <josephkyle.sumucad@cit.edu>
2025-11-21 12:02:30 +0800

    typo

--- backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/Controller/userController.java
+++ backend/RoadWatch/src/main/java/road/watch/it_342_g01/RoadWatch/Controller/userController.java
@@ -1,4 +1,4 @@
-package road.watch.it_342_g01.RoadWatch.Controller;
+package road.watch.it_342_g01.RoadWatch.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
